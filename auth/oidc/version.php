89df924a6db65961b11912f8d9aa5305326f58bc
adi8900 <adrianszymanski242@gmail.com>
2026-01-16 12:06:17 +0100

    moodle: Add msteams plugin and its dependency

--- auth/oidc/version.php
+++ auth/oidc/version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025040800;
+$plugin->version = 2025040815;
 $plugin->requires = 2025040800;
-$plugin->release = '5.0.0';
+$plugin->release = '5.0.4';
 $plugin->component = 'auth_oidc';
 $plugin->maturity = MATURITY_STABLE;
